c78cd9f35d3517b816c5040a320b63e56207b4b9
kuaileqiewuyou <145300260+kuaileqiewuyou@users.noreply.github.com>
2026-02-24 12:42:40 +0800

    add gif sequence writer

---
+++ src/main/java/com/example/gifmaker/util/GifSequenceWriter.java
@@ -0,0 +1,86 @@
+package com.example.gifmaker.util;
+
+import org.w3c.dom.Node;
+
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.metadata.IIOMetadataNode;
+import javax.imageio.stream.ImageOutputStream;
+import java.awt.image.RenderedImage;
+import java.io.Closeable;
+import java.io.IOException;
+import java.util.Iterator;
+
+public class GifSequenceWriter implements Closeable {
+
+    private final ImageWriter gifWriter;
+    private final ImageWriteParam imageWriteParam;
+    private final IIOMetadata imageMetaData;
+
+    public GifSequenceWriter(ImageOutputStream outputStream, int imageType, int delayMs, boolean loop) throws IOException {
+        gifWriter = getWriter();
+        imageWriteParam = gifWriter.getDefaultWriteParam();
+        ImageTypeSpecifier imageTypeSpecifier = ImageTypeSpecifier.createFromBufferedImageType(imageType);
+
+        imageMetaData = gifWriter.getDefaultImageMetadata(imageTypeSpecifier, imageWriteParam);
+        String metaFormatName = imageMetaData.getNativeMetadataFormatName();
+
+        IIOMetadataNode root = (IIOMetadataNode) imageMetaData.getAsTree(metaFormatName);
+
+        IIOMetadataNode graphicsControlExtensionNode = getNode(root, "GraphicControlExtension");
+        graphicsControlExtensionNode.setAttribute("disposalMethod", "none");
+        graphicsControlExtensionNode.setAttribute("userInputFlag", "FALSE");
+        graphicsControlExtensionNode.setAttribute("transparentColorFlag", "FALSE");
+        graphicsControlExtensionNode.setAttribute("delayTime", Integer.toString(Math.max(1, delayMs / 10)));
+        graphicsControlExtensionNode.setAttribute("transparentColorIndex", "0");
+
+        IIOMetadataNode appExtensionsNode = getNode(root, "ApplicationExtensions");
+        IIOMetadataNode appExtensionNode = new IIOMetadataNode("ApplicationExtension");
+
+        appExtensionNode.setAttribute("applicationID", "NETSCAPE");
+        appExtensionNode.setAttribute("authenticationCode", "2.0");
+
+        int loopFlag = loop ? 0 : 1;
+        byte[] loopBytes = new byte[]{0x1, (byte) (loopFlag & 0xFF), (byte) ((loopFlag >> 8) & 0xFF)};
+        appExtensionNode.setUserObject(loopBytes);
+        appExtensionsNode.appendChild(appExtensionNode);
+
+        imageMetaData.setFromTree(metaFormatName, root);
+
+        gifWriter.setOutput(outputStream);
+        gifWriter.prepareWriteSequence(null);
+    }
+
+    public void writeToSequence(RenderedImage image) throws IOException {
+        gifWriter.writeToSequence(new IIOImage(image, null, imageMetaData), imageWriteParam);
+    }
+
+    @Override
+    public void close() throws IOException {
+        gifWriter.endWriteSequence();
+    }
+
+    private static ImageWriter getWriter() {
+        Iterator<ImageWriter> iter = ImageIO.getImageWritersBySuffix("gif");
+        if (!iter.hasNext()) {
+            throw new IllegalStateException("No GIF Image Writers Exist");
+        }
+        return iter.next();
+    }
+
+    private static IIOMetadataNode getNode(IIOMetadataNode rootNode, String nodeName) {
+        int nNodes = rootNode.getLength();
+        for (int i = 0; i < nNodes; i++) {
+            if (rootNode.item(i).getNodeName().equalsIgnoreCase(nodeName)) {
+                return (IIOMetadataNode) rootNode.item(i);
+            }
+        }
+        IIOMetadataNode node = new IIOMetadataNode(nodeName);
+        rootNode.appendChild(node);
+        return node;
+    }
+}
