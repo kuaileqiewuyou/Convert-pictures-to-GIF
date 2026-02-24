df640d109cbb48e7326e1a31c9fc7360e737e90a
kuaileqiewuyou <145300260+kuaileqiewuyou@users.noreply.github.com>
2026-02-24 12:40:01 +0800

    add spring boot entry point

---
+++ src/main/java/com/example/gifmaker/GifMakerApplication.java
@@ -0,0 +1,12 @@
+package com.example.gifmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifMakerApplication.class, args);
+    }
+}
